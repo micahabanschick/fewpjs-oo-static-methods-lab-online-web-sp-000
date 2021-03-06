a3ce1c1291f9e041790c19c1445d77d761965bc2
Micah Banschick <micha.ban@gmail.com>
2021-03-06 02:46:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+       return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    static sanitize(str) {
+      str = str.replace(/[^'a-z0-9áéíóúñü \.,_-]/gim,"");
+    return str.trim();
+    }
+
+    static titleize(string) {
+      let arr = string.split(" ");
+      let newWord = [];
+      let okArr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      for (let i = 0; i < arr.length; i++) {
+        if (okArr.includes(arr[i]) && i > 0) {
+          newWord.push(arr[i])
+          continue;
+        }
+        newWord.push(this.capitalize(arr[i]))
+      }
+      return newWord.join(" ");
+    }
 }

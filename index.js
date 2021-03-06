951b9419c8484206d7ff11210f78914fdc155daf
Micah Banschick <micha.ban@gmail.com>
2021-03-06 02:47:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ class Formatter {
       for (let i = 0; i < arr.length; i++) {
         if (okArr.includes(arr[i]) && i > 0) {
           newWord.push(arr[i])
-          continue;
         }
         newWord.push(this.capitalize(arr[i]))
       }
